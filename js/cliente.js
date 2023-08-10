400cb5f4df55e3abd28351a15edca59b52f31ad1
M4ico <141885485+M4ico@users.noreply.github.com>
2023-08-10 02:51:15 -0600

    Update cliente.js

--- js/cliente.js
+++ js/cliente.js
@@ -258,7 +258,7 @@ function cargarTablaClientes() {
         datosTabla += "<td><a class='nav-link' href='#' style='color: black;'>" + clientes[i].telefono + "</a></td>";
         datosTabla += "<td><a class='nav-link' href='#' style='color: black;'>" + clientes[i].correoElectronico + "</a></td>";
         datosTabla += "<td><label class='switch'><input type='checkbox'><span class='slider round'></span></label></td>";
-        datosTabla += "<td><button class='botonessincontorno' class='btn btn-primary' type='button' data-bs-toggle='modal' data-bs-target='#myModalDos' onclick='modificarClientes(" + i + ");'><i class='fa-solid fa-pen-to-square fa-xl' style='color: #286c7d; margin-right: 20px; padding: 0; border:0; outline: none;'></i></button><button class='botonessincontorno' type='button' onclick='eliminarCliente(" + i + ");'><i class='fa-solid fa-trash fa-xl' style='color: #286c7d; padding: 0; border:0; outline: none;'></i></button></td>";
+        datosTabla += "<td><button class='botonessincontorno' class='btn btn-primary' type='button' data-bs-toggle='modal' data-bs-target='#myModalDos' onclick='modificarClientes(" + i + ");'><i class='fa-solid fa-pen-to-square fa-xl' style='color: #286c7d; margin-right: 20px; padding: 0; border:0; outline: none;'></i></button><button class='botonessincontorno' type='button' class='btn btn-primary' data-bs-toggle='modal' data-bs-target='#class='btn btn-primary' data-bs-toggle='modal' data-bs-target='#ModalEliminar' onclick='eliminarCliente(" + i + ");'><i class='fa-solid fa-trash fa-xl' style='color: #286c7d; padding: 0; border:0; outline: none;'></i></button></td>";
 
         datosTabla += "</tr>";
     }
